3c48246f9a6cacca4bfa6c0d6744e8b7bd5ab490
owusuvictorjnr <owusuvictor80@gmail.com>
2025-07-10 15:05:39 +0000

    user entity

---
+++ apps/api/src/user/user.entity.ts
@@ -0,0 +1,38 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+// This enum defines the possible roles a user can have.
+export enum UserRole {
+  ADMIN = "admin",
+  TEACHER = "teacher",
+  PARENT = "parent",
+  ACCOUNTANT = "accountant",
+  STUDENT = "student",
+}
+
+@Entity("users") // This decorator marks the class as a database table named 'users'.
+export class User {
+  @PrimaryGeneratedColumn("uuid") // Creates a primary key column that auto-generates a unique ID.
+  id: string;
+
+  @Column({ unique: true }) // Creates a column for the email, which must be unique.
+  email: string;
+
+  @Column() // Creates a column for the hashed password.
+  password: string;
+
+  @Column({
+    type: "enum",
+    enum: UserRole,
+    default: UserRole.PARENT, // Sets the default role for new users to 'parent'.
+  })
+  role: UserRole;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  middleName: string;
+
+  @Column()
+  lastName: string;
+}
