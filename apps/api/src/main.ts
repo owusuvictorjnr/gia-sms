4c255472b121f747c30988ef3be8bd0a839ec429
owusuvictorjnr <owusuvictor80@gmail.com>
2025-07-10 21:31:41 +0000

    add cors to allow req

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // We will add more configuration here later (e.g., CORS, validation pipes)
-  await app.listen(3001); // The backend will run on port 3001
+
+  // Enable CORS to allow requests from our frontend
+  app.enableCors({
+    origin: "http://localhost:3000", // Allow requests from our frontend's address
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  });
+
+  await app.listen(3001);
 }
 bootstrap();
