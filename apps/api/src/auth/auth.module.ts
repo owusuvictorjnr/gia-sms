8701e0a63f1119ad0acdefe11b53ec854ff68b3c
owusuvictorjnr <owusuvictor80@gmail.com>
2025-07-10 16:13:15 +0000

    add the strategies in the provider

---
+++ apps/api/src/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { Module } from "@nestjs/common";
+import { UserModule } from "../user/user.module";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { AuthService } from "./auth.service";
+import { LocalStrategy } from "./local.strategy";
+import { JwtStrategy } from "./jwt.strategy";
+
+@Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>("JWT_SECRET"),
+        signOptions: { expiresIn: "2h" },
+      }),
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy], // We've added the strategies here
+  exports: [AuthService],
+})
+export class AuthModule {}
