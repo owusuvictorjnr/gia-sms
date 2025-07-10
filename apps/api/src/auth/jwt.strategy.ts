8f7c331bb9e3f5dc6d8fad48324a6a0190ea3f14
owusuvictorjnr <owusuvictor80@gmail.com>
2025-07-10 16:12:15 +0000

    jwt strategy

---
+++ apps/api/src/auth/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { PassportStrategy } from "@nestjs/passport";
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>("JWT_SECRET"),
+    });
+  }
+
+  async validate(payload: any) {
+    // The payload is the decrypted JWT token.
+    // We return the user's ID, email, and role, which will be attached to the request object.
+    return { userId: payload.sub, email: payload.email, role: payload.role };
+  }
+}
