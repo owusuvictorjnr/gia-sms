e067311cc9ad876a2f058ae78abc96f086483d8d
owusuvictorjnr <owusuvictor80@gmail.com>
2025-07-12 12:41:18 +0000

    import  attendanceModule and attendance in the entities

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,40 +1,40 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
-import { User } from './user/user.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
-import { Grade } from './grade/grade.entity';
-import { GradeModule } from './grade/grade.module';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { UserModule } from "./user/user.module";
+import { AuthModule } from "./auth/auth.module";
+import { GradeModule } from "./grade/grade.module";
+import { User } from "./user/user.entity";
+import { Grade } from "./grade/grade.entity";
+import { AttendanceModule } from "./attendance/attendance.module";
+import { Attendance } from "./attendance/attendance.entity";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
+      envFilePath: ".env",
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
-        entities: [User, Grade],
+        type: "postgres",
+        host: configService.get<string>("DB_HOST"),
+        port: configService.get<number>("DB_PORT"),
+        username: configService.get<string>("DB_USERNAME"),
+        password: configService.get<string>("DB_PASSWORD"),
+        database: configService.get<string>("DB_DATABASE"),
+        entities: [User, Grade, Attendance],
         synchronize: true,
       }),
     }),
-    UserModule,
-    AuthModule, // We've imported the AuthModule here
-    GradeModule, // We've imported the GradeModule here
+    UserModule, // Added UserModule
+    AuthModule, // Added AuthModule
+    GradeModule, // Added GradeModule
+    AttendanceModule, // Added AttendanceModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
-
+  controllers: [], // Removed AppController
+  providers: [], // Removed AppService
 })
 export class AppModule {}
