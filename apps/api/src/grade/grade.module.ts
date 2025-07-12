cdd0225261197df21cbf27d6b46be72735e0128c
owusuvictorjnr <owusuvictor80@gmail.com>
2025-07-12 12:46:13 +0000

    he GradeModule is responsible for managing grades in the application

--- apps/api/src/grade/grade.module.ts
+++ apps/api/src/grade/grade.module.ts
@@ -2,12 +2,14 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Grade } from "./grade.entity";
 import { GradeService } from "./grade.service";
+import { GradeController } from "./grade.controller";
+import { AuthModule } from "src/auth/auth.module";
 
 // The GradeModule is responsible for managing grades in the application.
 @Module({
-  imports: [TypeOrmModule.forFeature([Grade])],
+  imports: [TypeOrmModule.forFeature([Grade]), AuthModule], // Import AuthModule to use the JwtAuthGuard
   providers: [GradeService], // We've added the GradeService here
   exports: [GradeService], // Also exporting it for other modules to use if needed
-  controllers: [], // We will add GradeController here soon
+  controllers: [GradeController], // Register the GradeController
 })
 export class GradeModule {}
