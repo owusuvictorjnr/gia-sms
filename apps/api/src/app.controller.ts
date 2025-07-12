7d9d3ed580a8a42d4a18d51af8b1e7a90a23e390
owusuvictorjnr <owusuvictor80@gmail.com>
2025-07-12 12:34:46 +0000

    added new endpoint to get all students and users

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  Get,
+  UseGuards,
+} from "@nestjs/common";
+import { UserService } from "./user/user.service";
+import { CreateUserDto } from "./user/dto/create-user.dto";
+import { JwtAuthGuard } from "./auth/jwt-auth.guard";
+import { AppService } from "./app.service";
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+@Controller("users")
+export class UserController {
+  constructor(private readonly userService: UserService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  async create(@Body(new ValidationPipe()) createUserDto: CreateUserDto) {
+    // We remove the password from the returned object for security
+    const { password, ...result } =
+      await this.userService.create(createUserDto);
+    return result;
+  }
+
+  // New endpoint to get all students
+  @Get("students")
+  @UseGuards(JwtAuthGuard) // Protect this route so only logged-in users can access it
+  async findAllStudents() {
+    const students = await this.userService.findAllStudents();
+    // Return the list of students, making sure to exclude their passwords
+    return students.map(({ password, ...rest }) => rest);
   }
 }
