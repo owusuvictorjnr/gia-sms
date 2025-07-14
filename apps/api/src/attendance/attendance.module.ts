65070b77e4215e71e533f71b66536edf977e3845
owusuvictorjnr <owusuvictor80@gmail.com>
2025-07-14 22:47:58 +0000

    The AttendanceModule is responsible for managing attendance-related functionality

--- apps/api/src/attendance/attendance.module.ts
+++ apps/api/src/attendance/attendance.module.ts
@@ -1,10 +1,19 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Attendance } from "./attendance.entity";
+import { AttendanceService } from "./attendance.service";
+import { AttendanceController } from "./attendance.controller";
+import { AuthModule } from "../auth/auth.module";
 
+
+// The AttendanceModule is responsible for managing attendance-related functionality
 @Module({
-  imports: [TypeOrmModule.forFeature([Attendance])],
-  providers: [], // We will add AttendanceService here soon
-  controllers: [], // We will add AttendanceController here soon
+  imports: [
+    TypeOrmModule.forFeature([Attendance]),
+    AuthModule, // Import AuthModule to use the JwtAuthGuard
+  ],
+  providers: [AttendanceService],
+  exports: [AttendanceService],
+  controllers: [AttendanceController], // Register the AttendanceController
 })
 export class AttendanceModule {}
