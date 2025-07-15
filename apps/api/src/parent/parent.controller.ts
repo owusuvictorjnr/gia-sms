ab58592a24c063104352517eeee7dc331bc3d1fc
owusuvictorjnr <owusuvictor80@gmail.com>
2025-07-15 16:02:17 +0000

    Get details of a specific child by ID

--- apps/api/src/parent/parent.controller.ts
+++ apps/api/src/parent/parent.controller.ts
@@ -1,19 +1,32 @@
-import { Controller, Get, UseGuards } from "@nestjs/common";
+import { Controller, Get, Param, UseGuards } from "@nestjs/common";
 import { JwtAuthGuard } from "../auth/jwt-auth.guard";
 import { User } from "../user/user.entity";
 import { ParentService } from "./parent.service";
 import { GetUser } from "src/auth/get-user.decorator";
 
-
-// This controller handles parent-related routes
+// ParentController handles routes related to parents
 @Controller("parent")
-@UseGuards(JwtAuthGuard) // Protect all routes in this controller
+@UseGuards(JwtAuthGuard)
 export class ParentController {
   constructor(private readonly parentService: ParentService) {}
 
   @Get("my-children")
   findMyChildren(@GetUser() parent: User) {
-    // Here you could add a check to ensure parent.role is 'parent'
     return this.parentService.findMyChildren(parent);
+  }
+
+  // Get details of a specific child by ID
+
+  @Get("child/:childId/grades")
+  getChildGrades(@GetUser() parent: User, @Param("childId") childId: string) {
+    return this.parentService.getChildGrades(parent, childId);
+  }
+
+  @Get("child/:childId/attendance-summary")
+  getChildAttendanceSummary(
+    @GetUser() parent: User,
+    @Param("childId") childId: string
+  ) {
+    return this.parentService.getChildAttendanceSummary(parent, childId);
   }
 }
