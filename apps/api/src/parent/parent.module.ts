233753c19386723002c8756fc51ee5d0d861d310
owusuvictorjnr <owusuvictor80@gmail.com>
2025-07-15 16:03:11 +0000

    import and use GradeModule and AttendanceModule

--- apps/api/src/parent/parent.module.ts
+++ apps/api/src/parent/parent.module.ts
@@ -4,6 +4,8 @@ import { User } from "../user/user.entity";
 import { ParentController } from "./parent.controller";
 import { ParentService } from "./parent.service";
 import { AuthModule } from "../auth/auth.module";
+import { GradeModule } from "src/grade/grade.module";
+import { AttendanceModule } from "src/attendance/attendance.module";
 
 
 // The ParentModule is responsible for managing parent-related functionality
@@ -11,6 +13,8 @@ import { AuthModule } from "../auth/auth.module";
   imports: [
     TypeOrmModule.forFeature([User]), // We need the User repository to find parents/children
     AuthModule, // We need this for the JwtAuthGuard
+     GradeModule, // Import GradeModule to use GradeService
+    AttendanceModule, // Import AttendanceModule to use AttendanceService
   ],
   controllers: [ParentController],
   providers: [ParentService],
