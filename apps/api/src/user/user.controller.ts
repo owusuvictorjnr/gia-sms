06ffeee52f72fc88a67fbb621b2649f1e00b47f0
owusuvictorjnr <owusuvictor80@gmail.com>
2025-07-16 07:42:15 +0000

    endpoint gets all users with the 'student' role

--- apps/api/src/user/user.controller.ts
+++ apps/api/src/user/user.controller.ts
@@ -1,15 +1,27 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
-@Controller('users') // This means all routes in this controller will start with /users
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post() // Handles POST requests to /users
+  // This endpoint is for creating new users (e.g., during registration)
+  @Post()
   async create(@Body(new ValidationPipe()) createUserDto: CreateUserDto) {
     // We remove the password from the returned object for security
     const { password, ...result } = await this.userService.create(createUserDto);
     return result;
+  }
+
+  // This endpoint gets all users with the 'student' role.
+  // It is used by admins/teachers to populate dropdowns.
+  @Get('students')
+  @UseGuards(JwtAuthGuard) // Protect this route
+  async findAllStudents() {
+    const students = await this.userService.findAllStudents();
+    // Return the list of students, making sure to exclude their passwords
+    return students.map(({ password, ...rest }) => rest);
   }
 }
