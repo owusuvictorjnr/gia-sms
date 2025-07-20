5e89a39e941cd9996eb7bf2ee99b99b852743001
owusuvictorjnr <owusuvictor80@gmail.com>
2025-07-20 20:15:44 +0000

    New method for general user search

--- apps/api/src/user/user.service.ts
+++ apps/api/src/user/user.service.ts
@@ -1,11 +1,9 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { Repository, Like, Not } from "typeorm";
 import { User, UserRole } from "./user.entity";
-import * as bcrypt from "bcryptjs"; // Changed from 'bcrypt' to 'bcryptjs'
+import * as bcrypt from "bcryptjs";
 
-
-// The UserService is responsible for managing user-related functionality, such as creating users and finding them by email.
 @Injectable()
 export class UserService {
   constructor(
@@ -14,15 +12,12 @@ export class UserService {
   ) {}
 
   async create(userData: Partial<User>): Promise<User> {
-    // Hash the password before saving
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(userData.password, salt);
-
     const newUser = this.usersRepository.create({
       ...userData,
       password: hashedPassword,
     });
-
     return this.usersRepository.save(newUser);
   }
 
@@ -30,11 +25,25 @@ export class UserService {
     return this.usersRepository.findOne({ where: { email } });
   }
 
-  // New method to find all students
   async findAllStudents(): Promise<User[]> {
     return this.usersRepository.find({
       where: { role: UserRole.STUDENT },
-      order: { lastName: "ASC", firstName: "ASC" }, // Order them alphabetically
+      order: { lastName: "ASC", firstName: "ASC" },
     });
+  }
+
+  // New method for general user search
+  async searchAllUsers(query: string, currentUserId: string): Promise<User[]> {
+    const users = await this.usersRepository.find({
+      where: [
+        { id: Not(currentUserId), email: Like(`%${query}%`) },
+        { id: Not(currentUserId), firstName: Like(`%${query}%`) },
+        { id: Not(currentUserId), middleName: Like(`%${query}%`) },
+        { id: Not(currentUserId), lastName: Like(`%${query}%`) },
+      ],
+      take: 10,
+    });
+    // Map the data to exclude sensitive information
+    return users.map(({ password, ...rest }) => rest as User);
   }
 }
