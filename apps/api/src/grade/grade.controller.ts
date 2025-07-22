a29a78e1750640ad3fe3080f8906cda5596ce38d
owusuvictorjnr <owusuvictor80@gmail.com>
2025-07-22 01:02:32 +0000

    Endpoint to update a grade

--- apps/api/src/grade/grade.controller.ts
+++ apps/api/src/grade/grade.controller.ts
@@ -3,38 +3,62 @@ import {
   Post,
   Body,
   Get,
+  Patch,
+  Delete,
   Param,
   UseGuards,
   ValidationPipe,
 } from "@nestjs/common";
 import { GradeService } from "./grade.service";
 import { CreateGradeDto } from "./dto/create-grade.dto";
+import { UpdateGradeDto } from "./dto/update-grade.dto";
 import { JwtAuthGuard } from "../auth/jwt-auth.guard";
-import { User } from "../user/user.entity";
+import { RolesGuard } from "../auth/guards/roles.guard";
+import { Roles } from "../auth/decorators/roles.decorator";
+import { UserRole } from "../user/user.entity";
 import { GetUser } from "src/auth/get-user.decorator";
 
-
 /**
- * GradeController handles requests related to student grades.
- * It allows teachers to create grades for students and retrieve grades for a specific student.
- * The controller uses the JwtAuthGuard to protect its routes, ensuring that only authenticated users can access them.
+ * GradeController handles all grade-related operations.
+ * It provides endpoints for creating, updating, retrieving, and deleting grades.
  */
 @Controller("grades")
-@UseGuards(JwtAuthGuard) // Protect all routes in this controller
+@UseGuards(JwtAuthGuard)
 export class GradeController {
   constructor(private readonly gradeService: GradeService) {}
 
   @Post()
+  @UseGuards(RolesGuard)
+  @Roles(UserRole.TEACHER)
   create(
     @Body(new ValidationPipe()) createGradeDto: CreateGradeDto,
-    @GetUser() user: any // Use 'any' to match the JWT payload shape
+    @GetUser() user: any
   ) {
-    // Here you might add a check to ensure user.role is 'teacher'
     return this.gradeService.create(createGradeDto, user);
   }
 
   @Get("/student/:studentId")
   findForStudent(@Param("studentId") studentId: string) {
     return this.gradeService.findForStudent(studentId);
+  }
+
+
+  // Endpoint to update a grade
+  @Patch(":id")
+  @UseGuards(RolesGuard)
+  @Roles(UserRole.TEACHER)
+  update(
+    @Param("id") id: string,
+    @Body(new ValidationPipe()) updateGradeDto: UpdateGradeDto,
+    @GetUser() user: { userId: string }
+  ) {
+    return this.gradeService.update(id, updateGradeDto, user.userId);
+  }
+
+  @Delete(":id")
+  @UseGuards(RolesGuard)
+  @Roles(UserRole.TEACHER)
+  remove(@Param("id") id: string, @GetUser() user: { userId: string }) {
+    return this.gradeService.remove(id, user.userId);
   }
 }
