a25c2473b35bd7798027a6e884e9d31a60956f29
owusuvictorjnr <owusuvictor80@gmail.com>
2025-07-22 01:00:45 +0000

    import TimetableEntry

--- apps/api/src/class/class.module.ts
+++ apps/api/src/class/class.module.ts
@@ -5,17 +5,17 @@ import { ClassService } from "./class.service";
 import { User } from "../user/user.entity";
 import { ClassController } from "./class.controller";
 import { AuthModule } from "../auth/auth.module";
+import { TimetableEntry } from "../timetable/timetable.entity"; // Import TimetableEntry
 
+// This module is responsible for managing classes in the application.
 
-// The ClassModule is responsible for managing class-related operations such as creating classes, assigning users to classes, and fetching class rosters for teachers.
-// It imports the necessary TypeORM modules for Class and User entities, and registers the ClassService
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Class, User]),
-    AuthModule, // Import AuthModule to use the JwtAuthGuard
+    TypeOrmModule.forFeature([Class, User, TimetableEntry]), // Add TimetableEntry here
+    AuthModule,
   ],
   providers: [ClassService],
   exports: [ClassService],
-  controllers: [ClassController], // Register the ClassController
+  controllers: [ClassController],
 })
 export class ClassModule {}
