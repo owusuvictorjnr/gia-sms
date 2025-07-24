868489bf07ce19d252c196c3fd4ea97b708832cc
owusuvictorjnr <owusuvictor80@gmail.com>
2025-07-24 21:18:52 +0000

    Add Class to the list of entities for this module

--- apps/api/src/admin/admin.module.ts
+++ apps/api/src/admin/admin.module.ts
@@ -6,12 +6,13 @@ import { AdminService } from "./admin.service";
 import { AuthModule } from "../auth/auth.module";
 import { Announcement } from "../announcement/announcement.entity";
 import { Transaction } from "../transaction/transaction.entity";
-import { Invoice } from "../finance/invoice.entity"; // Import Invoice
+import { Invoice } from "../finance/invoice.entity";
+import { Class } from "../class/class.entity";
 
 @Module({
   imports: [
     // Add Invoice to the list of entities for this module
-    TypeOrmModule.forFeature([User, Announcement, Transaction, Invoice]),
+    TypeOrmModule.forFeature([User, Announcement, Transaction, Invoice, Class]), // Add Class repository
     AuthModule,
   ],
   controllers: [AdminController],
